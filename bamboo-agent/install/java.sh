f8e22493ef28a2cc5bc0857da8307998007642ae
Nick Featherston <nick.featherston@sdspathology.com.au>
2016-05-20 08:47:24 +1000

    Change add-apt-repository to apt-add-repository

--- bamboo-agent/install/java.sh
+++ bamboo-agent/install/java.sh
@@ -4,7 +4,7 @@ set -e
 
 # Add webupd8team apt repsitory
 eatmydata -- apt-get install -yq software-properties-common
-add-apt-repository ppa:webupd8team/java -y && apt-get update
+apt-add-repository ppa:webupd8team/java -y && apt-get update
 
 # Install Oracle Java 8
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
