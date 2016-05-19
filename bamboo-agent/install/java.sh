81e1da5b4d3bae52cd232fd32e2934e9783a8a4e
Nick Featherston <nick.featherston@sdspathology.com.au>
2016-05-19 15:02:13 +1000

    Update to Java 8 and added latest MySQL connector

--- bamboo-agent/install/java.sh
+++ bamboo-agent/install/java.sh
@@ -6,6 +6,6 @@ set -e
 eatmydata -- apt-get install -yq python-software-properties
 add-apt-repository ppa:webupd8team/java -y && apt-get update
 
-# Install Oracle Java 7
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-eatmydata -- apt-get install -yq oracle-java7-installer
+# Install Oracle Java 8
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+eatmydata -- apt-get install -yq oracle-java8-installer
